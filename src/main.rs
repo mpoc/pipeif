22d74f19ee37270a822f3bdde3b41e6941f13450
Marius <marius.pocev@gmail.com>
2022-05-22 12:46:55 +0100

    Add basic command line options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{ArgEnum, Parser};
+
+/// Pipe stdin to stdout if stdin matches a hash
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// Expected hash of stdin file
+    #[clap(forbid_empty_values = true)]
+    hash: String,
+
+    /// The hashing algorithm used for the hash
+    #[clap(arg_enum, short, long)]
+    algorithm: Option<HashAlgorithm>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Debug)]
+enum HashAlgorithm {
+    Md5,
+    Sha256,
+    Sha512,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("name: {:?}", cli.hash);
+
+    if let Some(algorithm) = cli.algorithm {
+        println!("Value for algorithm: {:?}", algorithm);
+    }
 }
