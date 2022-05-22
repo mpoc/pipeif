5a1dcee5d26c25bc6380f325d4aee257b6e96fbe
Marius <marius.pocev@gmail.com>
2022-05-22 14:11:17 +0100

    Add sha256 and sha512 hashes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{io, io::prelude::*};
 
 use clap::{ArgEnum, Parser};
+use sha2::{Sha256, Sha512, Digest};
 
 /// Pipe stdin to stdout if stdin matches a hash
 #[derive(Parser, Debug)]
@@ -44,7 +45,17 @@ fn main() {
 
     let lines: String = io::stdin().lock().lines().fold(String::new(), |s, l| s + &l.unwrap() + "\n");
 
-    let digest = md5::compute(lines);
-    let digest_str = format!("{:x}", digest);
-    println!("{}", digest_str);
+    let md5_digest = md5::compute(&lines);
+    let md5_digest_str = format!("{:x}", md5_digest);
+    println!("{}", md5_digest_str);
+
+    let mut sha256_digest = Sha256::new();
+    sha256_digest.update(&lines);
+    let sha256_digest_str: String = format!("{:X}", sha256_digest.finalize());
+    println!("{}", sha256_digest_str);
+
+    let mut sha512_digest = Sha512::new();
+    sha512_digest.update(&lines);
+    let sha512_digest_str: String = format!("{:X}", sha512_digest.finalize());
+    println!("{}", sha512_digest_str);
 }
